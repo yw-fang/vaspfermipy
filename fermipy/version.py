8d02f3f098d18f89b0518c1ffe497ffe1d327000
yw-fang <fyuewen@gmail.com>
2019-05-29 12:33:04 +0900

    version info.

---
+++ fermipy/version.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+__version__ = '1.0.0'
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__license__ = 'MIT License'
+__creation_date__ = "May 27th, 2019"
+__release_data__= 'Undetermined, 2019'
