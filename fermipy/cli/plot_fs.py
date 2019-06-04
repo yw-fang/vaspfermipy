142277f8ec8de94ba048f4a90b906753892d38f9
yw-fang <fyuewen@gmail.com>
2019-06-04 12:20:58 +0900

    create plot_fs.py file #2

---
+++ fermipy/cli/plot_fs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2018, Yue-Wen FANG.
+License: MIT License (see LICENSE for details)
+"""
+
+__author__ = "Yue-Wen FANG"
+__maintainer__ = "Yue-Wen FANG"
+__email__ = "fyuewen@gmail.com"
+__status__ = "Development"
+__creation_date__ = "June 4th, 2019"
+
+"""
+Plot Fermi surface using Xcrysden or VESTA.
+If these softwares are not installed, only the corresponding files
+are generated in the local directory.
+"""
+
