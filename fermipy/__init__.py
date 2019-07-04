4ca8ddfe842e52ef70ede88af00ce62970badef5
yw-fang <fyuewen@gmail.com>
2019-07-04 19:37:52 +0900

    remove

--- fermipy/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-from fermipy.version import __version__
-from fermipy.version import __author__
-from fermipy.version import __maintainer__
-from fermipy.version import __release_data__
-from fermipy.version import __license__
-
-# __all__ = ['fermipy']
