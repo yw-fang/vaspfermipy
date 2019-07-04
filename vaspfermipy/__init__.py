77d8d3b03eef20d7faba49fac8b0be00d793858e
yw-fang <fyuewen@gmail.com>
2019-07-04 19:40:41 +0900

    update init

--- vaspfermipy/__init__.py
+++ vaspfermipy/__init__.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
-from fermipy.version import __version__
-from fermipy.version import __author__
-from fermipy.version import __maintainer__
-from fermipy.version import __release_data__
-from fermipy.version import __license__
+from vaspfermipy.version import __version__
+from vaspfermipy.version import __author__
+from vaspfermipy.version import __maintainer__
+from vaspfermipy.version import __release_data__
+from vaspfermipy.version import __license__
 
-# __all__ = ['fermipy']
+# __all__ = ['vaspfermipy']
